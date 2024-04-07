08a3e7abb0e1ba80106e5f2ff01484fbd40e10a3
JT Smith <jt@plainblack.com>
2024-04-07 00:15:16 -0500

    fix missing session

--- server/api/v1/game/[id].delete.mjs
+++ server/api/v1/game/[id].delete.mjs
@@ -1,11 +1,12 @@
 import { useKind } from '#ving/record/utils.mjs';
 import { obtainSession, describeParams } from '#ving/utils/rest.mjs';
-import {defineEventHandler, getRouterParams} from 'h3';
+import { defineEventHandler, getRouterParams } from 'h3';
 export default defineEventHandler(async (event) => {
     const games = await useKind('Game');
     const { id } = getRouterParams(event);
     const game = await games.findOrDie(id);
-    game.canEdit(obtainSession(event));
+    const session = obtainSession(event);
+    game.canEdit(session);
     await game.delete();
     return game.describe(describeParams(event, session));
 });
