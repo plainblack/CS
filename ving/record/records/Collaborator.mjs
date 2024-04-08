e3d35f8e4603fead26c9937256e7b6e0cd065be5
JT Smith <jt@plainblack.com>
2024-04-08 08:21:32 -0500

    added collaborator record

---
+++ ving/record/records/Collaborator.mjs
@@ -0,0 +1,19 @@
+import { VingRecord, VingKind } from "#ving/record/VingRecord.mjs";
+import { eq } from '#ving/drizzle/orm.mjs';
+
+/** Management of individual Collaborators.
+ * @class
+ */
+export class CollaboratorRecord extends VingRecord {
+    // add custom Record code here
+
+
+}
+
+/** Management of all Collaborators.
+ * @class
+ */
+export class CollaboratorKind extends VingKind {
+    // add custom Kind code here
+
+}
