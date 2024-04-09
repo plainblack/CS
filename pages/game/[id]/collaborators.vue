efec78d177de0836d1fbbc36f08d171f39b1050a
JT Smith <jt@plainblack.com>
2024-04-08 22:09:09 -0500

    list collaborators

--- pages/game/[id]/collaborators.vue
+++ pages/game/[id]/collaborators.vue
@@ -9,8 +9,64 @@
 
                     <div class="flex gap-5 flex-column-reverse md:flex-row">
                         <div class="flex-auto p-fluid">
+                            <h2>Existing Collaborators</h2>
+
+                            <DataTable :value="collaborators.records" stripedRows
+                                @sort="(e) => collaborators.sortDataTable(e)">
+                                <Column field="props.userId" header="User Id" sortable>
+                                    <template #body="slotProps">
+                                        <UserProfile :user="slotProps.data.related?.user" />
+                                    </template>
+                                </Column>
+                                <Column field="props.createdAt" header="Created At" sortable>
+                                    <template #body="slotProps">
+                                        {{ dt.formatDateTime(slotProps.data.props.createdAt) }}
+                                    </template>
+                                </Column>
+                                <Column header="Remove">
+                                    <template #body="slotProps">
+                                        <Button v-if="slotProps.data.meta?.isOwner" title="Remove"
+                                            alt="Remove Collaborator" icon="pi pi-trash" severity="danger"
+                                            @click="slotProps.data.delete()" />
+                                    </template>
+                                </Column>
+                            </DataTable>
+                            <Pager :kind="collaborators" />
 
 
+                        </div>
+                    </div>
+                </div>
+
+                <div class="surface-card p-5 border-1 surface-border border-round mt-5">
+
+                    <div class="flex gap-5 flex-column-reverse md:flex-row">
+                        <div class="flex-auto p-fluid">
+
+
+                            <h2 class="mt-0">Add a Collaborator</h2>
+
+                            <Form :send="() => collaborators.create()">
+                                <div class="flex gap-5 flex-column-reverse md:flex-row">
+                                    <div class="flex-auto p-fluid">
+
+                                        <div class="mb-4">
+                                            <FormInput name="gameId" type="text" v-model="collaborators.new.gameId"
+                                                required label="Game Id" />
+                                        </div>
+                                        <div class="mb-4">
+                                            <FormInput name="userId" type="text" v-model="collaborators.new.userId"
+                                                required label="User Id" />
+                                        </div>
+                                        <div>
+                                            <Button type="submit" class="w-auto" severity="success">
+                                                <i class="pi pi-plus mr-1"></i> Create Collaborator
+                                            </Button>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </Form>
 
 
                         </div>
@@ -29,6 +85,7 @@ definePageMeta({
 });
 const route = useRoute();
 const id = route.params.id.toString();
+const currentUser = useCurrentUserStore();
 const game = useVingRecord({
     id,
     fetchApi: `/api/${restVersion()}/game/${id}`,
@@ -37,7 +94,19 @@ const game = useVingRecord({
         await navigateTo('/game');
     },
 });
-await game.fetch();
+const collaborators = useVingKind({
+    listApi: `/api/${restVersion()}/collaborator`,
+    createApi: `/api/${restVersion()}/collaborator`,
+    query: { includeMeta: true, sortBy: 'createdAt', sortOrder: 'desc', includeRelated: ['user'] },
+    newDefaults: { gameId: id, userId: currentUser.props?.id },
+});
+await Promise.all([
+    game.fetch(),
+    collaborators.search(),
+    collaborators.fetchPropsOptions(),
+]);
+onBeforeRouteLeave(() => collaborators.dispose());
+
 const dt = useDateTime();
 
 </script>
