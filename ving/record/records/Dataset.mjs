72c8a5d7ad15b3b94e80ab90051ad0cfcb6670b5
JT Smith <jt@plainblack.com>
2024-04-11 19:13:58 -0500

    built custom describe

--- ving/record/records/Dataset.mjs
+++ ving/record/records/Dataset.mjs
@@ -1,63 +1,60 @@
 import { VingRecord, VingKind } from "#ving/record/VingRecord.mjs";
-import {eq} from '#ving/drizzle/orm.mjs';
+import { eq } from '#ving/drizzle/orm.mjs';
 
 /** Management of individual Datasets.
  * @class
  */
 export class DatasetRecord extends VingRecord {
     // add custom Record code here
-    
-  /**
-   * Extends `describe()` in `VingRecord` to add `meta` property `bar`
-   *  and the `extra` property `foo`.
-   * 
-   * Note, these don't do anything, this is just here to explain how to extend
-   * the describe method.
-   * 
-   * @see VingRecord.describe()
-   */
-  async describe(params = {}) {
-      const out = await super.describe(params);
-      if (params?.include?.meta && out.meta) {
-          out.meta.bar = 'bar';
-      }
-      if (params?.include?.extra.includes('foo')) {
-          if (out.extra === undefined) {
-              out.extra = {};
-          }
-          out.extra.foo = 'foo';
-      }
-      return out;
-  }
-  
-    
-        /**
-             * Extends `delete()` in `VingRecord` to delete the associated `Row`.
-             * 
-             * @see VingRecord.delete()
-             */
-        async delete () {
-            
-            await (await this.children('rows')).deleteMany();
-            
-            await super.delete();
+
+
+    async describe(params = {}) {
+        const out = await super.describe(params);
+        if (params?.include?.extra.includes('sumQuantity')) {
+            if (out.extra === undefined) {
+                out.extra = {};
+            }
+            out.extra.sumQuantity = 0;
+            const rows = await this.children('rows');
+            if (this.enumerateOn) {
+                const rowRecords = await rows.findMany();
+                for (const row of rowRecords) {
+                    let enumString = '';
+                    if (row.fields[this.enumerateOn]?.userValue) {
+                        enumString = row.fields[this.enumerateOn]?.userValue;
+                    }
+                    const enumerations = enumString.split(',');
+                    const enumCount = enumerations.length || 1;
+                    out.extra.sumQuantity += row.quantity * enumCount;
+                }
+            }
+            else {
+                out.extra.sumQuantity = await rows.sum('quantity');
+            }
         }
-        
+        if (params?.include?.extra.includes('rowCount')) {
+            if (out.extra === undefined) {
+                out.extra = {};
+            }
+            out.extra.rowCount = await (await this.children('rows')).count();
+        }
+        return out;
+    }
+
+    async delete() {
+
+        //  if (await (await this.parent('design')).count())
+        //      throw ouch(441, 'Cannot delete a data set while it is still attached to a design.');
+        await (await this.children('rows')).deleteMany();
+
+        await super.delete();
+    }
+
 }
 
 /** Management of all Datasets.
  * @class
  */
-export class DatasetKind extends VingKind  {
+export class DatasetKind extends VingKind {
     // add custom Kind code here
-    
-    /**
-     * An example of a shortcut for a common query you might want to make.
-     * 
-     * @returns a list of `DatasetRecord`s that are cool
-     */
-    async findCool() {
-        return this.select.findMany(eq(this.table.isCool, true));
-    }
-    
 }
